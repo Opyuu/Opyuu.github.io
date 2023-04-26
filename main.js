97e8d46bec3d46bb1c5acd1a1ed9a42c1c6a570f
Opyu <wayne.jiawei.shi@gmail.com>
2023-04-26 16:47:51 +0100

    draw green square on board

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //main.js
 
 const canvas = document.getElementById('board');
-const ctx = canvas.getContext('2d'); //idk if this works
+const ctx = canvas.getContext('2d');
 
 ctx.canvas.width = COLUMNS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100); // try drawing a green square to test or sth
