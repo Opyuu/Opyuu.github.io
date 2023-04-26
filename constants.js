faeb5ad054813628cb86db5860d2a9488d499567
Opyu <wayne.jiawei.shi@gmail.com>
2023-04-26 20:02:34 +0100

    FIX COLS

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 //constants.js
 const COLS = 10;
 const ROWS = 20;
-const BLOCK_SIZE = 30; //30x30 blocks
+const BLOCK_SIZE = 30; //30px * 30px blocks
 
 const INT_TO_PIECE = ["-", "I", "O", "J", "L", "S", "Z", "T", "#"];
 const PIECE_COLOUR = ["#000000", "#00ffe1", "#ffea00", "#0008ff", "#ff8800", "#00ff15", "#ff000d", "#b300ff", "#8f8f8f"]
--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 
-ctx.canvas.width = COLUMNS * BLOCK_SIZE;
+ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
