ee9cbc9737e6a2c690f1507ec4429903029fdcec
Opyu <wayne.jiawei.shi@gmail.com>
2023-04-26 16:41:09 +0100

    attempt to fix board #2

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
 //constants.js (oh// was js)
 const COLUMNS = 10;
 const ROWS = 20;
+const BLOCK_SIZE = 20;
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //main.js
 
 const canvas = document.getElementById('board');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d'); //idk if this works
+
+ctx.canvas.width = COLUMNS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
