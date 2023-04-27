998768fd98e7050e1042d8c33ad557fda8006f6d
Kixenon <97026746+Kixenon@users.noreply.github.com>
2023-04-27 15:43:55 +0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,9 +1,13 @@
 //constants.js
 const COLS = 10;
-const ROWS = 22;
+const ROWS = 24;
 const BLOCK_SIZE = 30;
+const BORDER_SIZE = 5;
+const GRID_SIZE = 1;
 
 const INT_TO_PIECE = ["-", "I", "O", "J", "L", "S", "Z", "T", "#"];
 //"#000000", "#00FFE1", "#FFEA00", "#0008FF", "#FF8800", "#00FF15", "#FF000D", "#B300FF", "#8F8F8F"
 const PIECE_COLOUR = ["#000000", "#00FFE1", "#FFEA00", "#0008FF", "#FF8800", "#00FF15", "#FF000D", "#B300FF", "#8F8F8F"]
 const GRID_COLOUR = "rgba(172, 170, 170, 1)";
+const BACKGROUND_COLOUR = "#000000";
+const BORDER_COLOUR = "#FFFFFF";
