c48e1677b7e0010c4279f009ad4626bdda833700
Kixenon <nothing.to.see.here926@gmail.com>
2023-04-28 18:43:29 +0800

    added queue, fixed 7bag, clean up, render fix

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ function play() {
     game = new Game();
     game.init();
     game.renderBoard();
-    game.x = -1;
-    game.y = 3;
-    game.piece = 1;
-    game.rotation = 3;
-    game.placePiece();
-    //game.renderPiece();
-    //game.clearPiece();
+    game.renderQueue();
 }
 
