01727bd458fc623118aa846322dc4f5f4e25caa7
Opyu <wayne.jiawei.shi@gmail.com>
2023-05-21 23:47:11 +0100

    Randomise 404 page image

---
+++ kanade.js
@@ -0,0 +1,85 @@
+const data = [
+    {
+        "LINK": "https://cdn.donmai.us/original/2d/11/__cinnamoroll_and_yoisaki_kanade_project_sekai_and_1_more_drawn_by_waka_wk4444__2d11e961551badd157dd7cbf0cb5cf76.jpg",
+        "SOURCE": "https://twitter.com/wk4444_",
+        "NAME": "wk4444"
+    },
+
+    {
+        "LINK": "Kanade_EN_Weekly_Promo.webp",
+        "SOURCE": "https://twitter.com/kheng_prsk",
+        "NAME": "Kheng"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/2d/75/__yoisaki_kanade_project_sekai_and_1_more_drawn_by_sleeper_am03__2d754704d92daeacaba25b915724f903.jpg",
+        "SOURCE": "https://twitter.com/sleeper_am03",
+        "NAME": "AM3"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/71/f9/__yoisaki_kanade_project_sekai_drawn_by_gin_gin937__71f95df45283a0024ca27713449bedeb.jpg",
+        "SOURCE": "https://www.pixiv.net/en/users/65620421",
+        "NAME": "GIN"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/f0/10/__yoisaki_kanade_project_sekai_drawn_by_jimmy_madomagi__f01008b1224e3dc2c93ff8da193769e0.jpg",
+        "SOURCE": "https://www.pixiv.net/en/users/5768971",
+        "NAME": "jimmy"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/9c/44/__yoisaki_kanade_project_sekai_and_1_more_drawn_by_shityu__9c44202fddd9877dfe9029177bb8f136.jpg",
+        "SOURCE": "https://twitter.com/shityu_france",
+        "NAME": "shityu"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/cb/5f/__yoisaki_kanade_project_sekai_drawn_by_waka_wk4444__cb5f8ee8fef0025884415ba26a4bbbf6.jpg",
+        "SOURCE": "https://twitter.com/wk4444_",
+        "NAME": "wk4444"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/3c/4c/__yoisaki_kanade_project_sekai_drawn_by_waka_wk4444__3c4ca0b859bc64bc31e324fcbd2c4b78.jpg",
+        "SOURCE": "https://twitter.com/wk4444_",
+        "NAME": "wk4444"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/0d/66/__yoisaki_kanade_project_sekai_drawn_by_tsune_tune__0d666e0092e1ef4e23c529a7ed64388d.png",
+        "SOURCE": "https://www.pixiv.net/en/users/11365815",
+        "NAME": "tsune"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/ef/49/__yoisaki_kanade_project_sekai_drawn_by_necojishi__ef491bbc5f7e92cffce03c54a0097460.jpg",
+        "SOURCE": "https://www.pixiv.net/en/users/12762068",
+        "NAME": "negojishi"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/8a/25/__shinonome_ena_akiyama_mizuki_asahina_mafuyu_and_yoisaki_kanade_project_sekai_drawn_by_waka_wk4444__8a25468b24d17a71955970cc45d52526.jpg",
+        "SOURCE": "https://twitter.com/wk4444_",
+        "NAME": "wk4444"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/ac/bb/__yoisaki_kanade_project_sekai_drawn_by_723_nanahumi__acbb9fde67212801b65402c7c6ca33e8.png",
+        "SOURCE": "https://www.pixiv.net/en/users/6697432",
+        "NAME": "Lena"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/fe/c8/__yoisaki_kanade_project_sekai_drawn_by_deadnooodles__fec85b296e881ce13a87010a736c3863.jpg",
+        "SOURCE": "https://www.pixiv.net/en/users/4077126",
+        "NAME": "deadnoodles"
+    },
+
+    {
+        "LINK": "https://cdn.donmai.us/original/5b/82/__yoisaki_kanade_project_sekai_drawn_by_qi_e_penguin__5b82e4f9d43b741ae7af909c39d34d53.jpg",
+        "SOURCE": "https://twitter.com/moyupenguin",
+        "NAME": "Moyu"
+    }
+]
